8dcec90d61f9b0bd8d8dae5191489c4a758a4997
Ombosoft <ombosoft@gmail.com>
2023-08-06 17:55:01 +0100

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
-import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 import "./App.css";
-import logo from "./logo.svg";
+
+const theme = createTheme({
+    typography: {
+        allVariants: {
+            fontFamily: "Nunito",
+        },
+    },
+});
 
 const App = () => {
-    const [count, setCount] = useState(0);
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Vite + React</h1>
-                <p>
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.jsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://react.dev"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {" | "}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className="App">Hello</div>
+        </ThemeProvider>
     );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { checker } from "vite-plugin-checker";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), checker()],
     test: {
         globals: true,
         environment: "jsdom",
